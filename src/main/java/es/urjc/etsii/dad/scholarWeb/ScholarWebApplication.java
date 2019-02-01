a28bdc653b03ad693b9aeebbd7ce3bcc9b48b1b0
belenp97 <belenpalazon.p@gmail.com>
2019-02-01 10:24:37 +0100

    Warning Mustache.

---
+++ src/main/java/es/urjc/etsii/dad/scholarWeb/ScholarWebApplication.java
@@ -0,0 +1,14 @@
+package es.urjc.etsii.dad.scholarWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarWebApplication.class, args);
+	}
+	
+}
+
