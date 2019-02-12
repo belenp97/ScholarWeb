3fbb57042a975e675f7eb30528e9899984648767
jorge <jorgealonso793@gmail.com>
2019-02-12 18:08:41 +0100

    organizacion

--- src/main/java/es/urjc/etsii/dad/scholarWeb/Repositories/PadreRepository.java
+++
@@ -1,9 +0,0 @@
-package es.urjc.etsii.dad.scholarWeb.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.urjc.etsii.dad.scholarWeb.Padre;
-
-public interface PadreRepository extends JpaRepository<Padre, String> {
-
-}
