a999b2dfcfb18280643421f31f3ffd022c7da03e
jorge <jorgealonso793@gmail.com>
2019-02-12 10:40:17 +0100

    Noticias
    he comentado las noticias del PrincipalControler para ver si podia enlazarse con NoticiasController

--- src/main/java/es/urjc/etsii/dad/scholarWeb/PrincipalControler.java
+++ src/main/java/es/urjc/etsii/dad/scholarWeb/PrincipalControler.java
@@ -1,8 +1,5 @@
 package es.urjc.etsii.dad.scholarWeb;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +7,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PrincipalControler {
-
-	private List<Noticia> noticias = new ArrayList<>();
-
+	
+	
 	public PrincipalControler() {
-		noticias.add(new Noticia("Saludos",
-				"Bienvenidos a Scholar Web, un colegio a las afueras de Madrid que quiere implantar una nueva mentalidad dentro de los colegios de la comunidad. Esperamos que para cualquier problema o mejora, nos lo hagan llegar mediante pagina de contacto. Un saludo y gracias"));
-		noticias.add(
-				new Noticia("Ultimas Noticias", "Todos los padres recibiran por mensaje, las notas de los alumnos"));
+		
 	}
 
 	@GetMapping("/")
@@ -26,11 +19,11 @@ public class PrincipalControler {
 		return "principal";
 	}
 
-	@GetMapping("/noticias")
+	/*@GetMapping("/noticias")
 	public String verNoticia(Model model) {
 
 		return "noticias";
-	}
+	}*/
 	
 	@GetMapping("/profesores")
 	public String verprofesores(Model model) {
