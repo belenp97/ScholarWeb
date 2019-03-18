06a343843e53cf71a988d405f9eeef625d407f1f
belenp97 <belenpalazon.p@gmail.com>
2019-03-18 15:52:28 +0100

    Descarga de PDF.
    -PDF comida.
    -PDF deportes.
    -PDF horario.

--- src/main/java/es/urjc/etsii/dad/scholarWeb/PrincipalControler.java
+++ src/main/java/es/urjc/etsii/dad/scholarWeb/PrincipalControler.java
@@ -1,13 +1,24 @@
 package es.urjc.etsii.dad.scholarWeb;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import es.urjc.etsii.dad.scholarWeb.Repositories.AlumnoRepository;
 import es.urjc.etsii.dad.scholarWeb.Repositories.AsignaturaRepository;
@@ -19,6 +30,9 @@ import es.urjc.etsii.dad.scholarWeb.Usuario;
 
 @Controller
 public class PrincipalControler {
+	
+	//Cambiar el path según donde se encuentre el proyecto.
+	private static final String EXTERNAL_FILE_PATH = "C:/Users/Belen/git/ScholarWeb/src/main/resources/";
 //
 //	@Autowired
 //	private AlumnoRepository reposAl;
@@ -128,6 +142,126 @@ public class PrincipalControler {
 //		model.addAttribute("admin",usuario.getRol());
 		
 		return "principal";
+	}
+	
+	@RequestMapping("/comida")
+	public void download (HttpServletRequest request, HttpServletResponse response,
+			@RequestParam(defaultValue="comedor.pdf") String fileName ) throws IOException
+	{
+		File file = new File(EXTERNAL_FILE_PATH +"/"+ fileName);
+			if (file.exists()) {
+
+				//get the mimetype
+				String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+				if (mimeType == null) {
+					//unknown mimetype so set the mimetype to application/octet-stream
+					mimeType = "application/pdf";
+				}
+
+				response.setContentType(mimeType);
+
+				
+
+				/**
+				 * Mencionamos el inline (si no está comentado el archivo se abrirá sin descargarse)
+				 */
+				response.setHeader("Content-Disposition", String.format("inline; filename=\"" + file.getName() + "\""));
+
+				 //en este caso tenemos attachment que descargrá directamente el pdf.
+				 response.setHeader("Content-Disposition", String.format("attachment; filename=\"" + file.getName() + "\""));
+
+				response.setContentLength((int) file.length());
+
+				InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+
+				FileCopyUtils.copy(inputStream, response.getOutputStream());
+
+			}
+		
+	}
+	
+	@RequestMapping("/deportes")
+	public void download2 (HttpServletRequest request, HttpServletResponse response,
+			@RequestParam(defaultValue="deportes.pdf") String fileName ) throws IOException
+	{
+		File file = new File(EXTERNAL_FILE_PATH +"/"+ fileName);
+			if (file.exists()) {
+
+				//get the mimetype
+				String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+				if (mimeType == null) {
+					//unknown mimetype so set the mimetype to application/octet-stream
+					mimeType = "application/pdf";
+				}
+
+				response.setContentType(mimeType);
+
+				/**
+				 * In a regular HTTP response, the Content-Disposition response header is a
+				 * header indicating if the content is expected to be displayed inline in the
+				 * browser, that is, as a Web page or as part of a Web page, or as an
+				 * attachment, that is downloaded and saved locally.
+				 * 
+				 */
+
+				/**
+				 * Here we have mentioned it to show inline
+				 */
+				response.setHeader("Content-Disposition", String.format("inline; filename=\"" + file.getName() + "\""));
+
+				 //Here we have mentioned it to show as attachment
+				 //response.setHeader("Content-Disposition", String.format("attachment; filename=\"" + file.getName() + "\""));
+
+				response.setContentLength((int) file.length());
+
+				InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+
+				FileCopyUtils.copy(inputStream, response.getOutputStream());
+
+			}
+		
+	}
+	
+	@RequestMapping("/horario")
+	public void download3 (HttpServletRequest request, HttpServletResponse response,
+			@RequestParam(defaultValue="horario.pdf") String fileName ) throws IOException
+	{
+		File file = new File(EXTERNAL_FILE_PATH +"/"+ fileName);
+			if (file.exists()) {
+
+				//get the mimetype
+				String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+				if (mimeType == null) {
+					//unknown mimetype so set the mimetype to application/octet-stream
+					mimeType = "application/pdf";
+				}
+
+				response.setContentType(mimeType);
+
+				/**
+				 * In a regular HTTP response, the Content-Disposition response header is a
+				 * header indicating if the content is expected to be displayed inline in the
+				 * browser, that is, as a Web page or as part of a Web page, or as an
+				 * attachment, that is downloaded and saved locally.
+				 * 
+				 */
+
+				/**
+				 * Here we have mentioned it to show inline
+				 */
+				response.setHeader("Content-Disposition", String.format("inline; filename=\"" + file.getName() + "\""));
+
+				 //Here we have mentioned it to show as attachment
+				 //response.setHeader("Content-Disposition", String.format("attachment; filename=\"" + file.getName() + "\""));
+
+				response.setContentLength((int) file.length());
+
+				InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+
+				FileCopyUtils.copy(inputStream, response.getOutputStream());
+
+			}
+		
 	}
 	
 //	public void modelos(Model model) {
