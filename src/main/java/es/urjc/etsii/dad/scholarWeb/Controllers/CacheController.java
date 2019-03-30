7aca28fd204a750566b1fb94d74818fef11cb4e4
belenp97 <belenpalazon.p@gmail.com>
2019-03-30 14:18:40 +0100

    Crontolador Caché

---
+++ src/main/java/es/urjc/etsii/dad/scholarWeb/Controllers/CacheController.java
@@ -0,0 +1,25 @@
+package es.urjc.etsii.dad.scholarWeb.Controllers;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CacheController {
+	@Autowired
+	private CacheManager cacheManager;
+	
+	@RequestMapping(value="/cache", method=RequestMethod.GET)
+	public Map<Object, Object> getCacheContent() {
+		ConcurrentMapCacheManager manager = (ConcurrentMapCacheManager) cacheManager;
+		ConcurrentMapCache cache = (ConcurrentMapCache) manager.getCache("titulaciones");
+		return cache.getNativeCache();
+	}
+}
